9e9693757390fbefe10097d607af56edf2f9d020
patrickkeogh <patrickkeogh@kantechprogramming.com>
2017-05-18 15:22:00 -0400

    Initial commit

---
+++ app/src/main/java/com/programming/kantech/mygathering/utils/Constants.java
@@ -0,0 +1,9 @@
+package com.programming.kantech.mygathering.utils;
+
+/**
+ * Created by patrick keogh on 2017-05-18.
+ */
+
+public class Constants {
+
+}
---
+++ app/src/main/java/com/programming/kantech/mygathering/view/activities/Activity_Main.java
@@ -0,0 +1,15 @@
+package com.programming.kantech.mygathering.view.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.programming.kantech.mygathering.R;
+
+public class Activity_Main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
